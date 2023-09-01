94ee73e36a18b6164c621cfa79ac2b5083cbad0a
Huỳnh Nguyễn Tuấn Duy <andyhntd2003@gmail.com>
2023-09-01 17:51:39 +0700

    update games

--- introduction_to_swift/basic_loops.playground/Contents.swift
+++ introduction_to_swift/basic_loops.playground/Contents.swift
@@ -19,3 +19,20 @@ repeat {
   secondDice = Int.random(in: 1...6)
 } while firstDice != secondDice
 print("It takes you \(times) times to roll a double dice with the value of \(firstDice)")
+
+//Create a game restriction
+ let levels = 10
+let bonusLevel = 3
+let freeLevels = 4
+for level in 1...levels {
+  
+    if level == bonusLevel {
+        print("Skipped bonus level 3")
+        continue
+    }
+    print("Level played \(level)")
+    if level == freeLevels {
+        print("You have reached maximum \(freeLevels) free levels, buy the game to play the other \(level-freeLevels)")
+        break
+    }
+}
