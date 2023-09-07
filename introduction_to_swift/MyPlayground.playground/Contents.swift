ed464e449911d1d9468f1e03a57d2ccb3ea1f6d9
Huỳnh Nguyễn Tuấn Duy <andyhntd2003@gmail.com>
2023-09-08 00:01:33 +0700

    mutating

--- introduction_to_swift/MyPlayground.playground/Contents.swift
+++ introduction_to_swift/MyPlayground.playground/Contents.swift
@@ -1,1 +1,14 @@
+struct TableReservation {
+  var name: String
+  var tableNumber: Int
+
+  mutating func updateBooking(_ updatedName: String) {
+      name = updatedName
+  }
+    
+}
+var reserve = TableReservation(name: "Matcha", tableNumber: 1)
+print(reserve)
+reserve.updateBooking("Dau Tay")
+print(reserve)
 
