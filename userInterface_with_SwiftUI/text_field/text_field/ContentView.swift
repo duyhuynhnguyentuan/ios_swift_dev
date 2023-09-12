679db3c27e2fc2c982530ffba4e0fd59963aa09a
Huỳnh Nguyễn Tuấn Duy <andyhntd2003@gmail.com>
2023-09-13 00:25:45 +0700

    text field

---
+++ userInterface_with_SwiftUI/text_field/text_field/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  text_field
+//
+//  Created by Duy Huỳnh Nguyễn Tuấn on 13/09/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+       ReservationForm()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ userInterface_with_SwiftUI/text_field/text_field/text_fieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  text_fieldApp.swift
+//  text_field
+//
+//  Created by Duy Huỳnh Nguyễn Tuấn on 13/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct text_fieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
