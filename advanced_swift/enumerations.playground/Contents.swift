8fac87915e28fc0608e2f4f96d7e94da68e966e9
Huỳnh Nguyễn Tuấn Duy <andyhntd2003@gmail.com>
2023-09-23 22:37:55 +0700

    Work with raw and associated values

--- advanced_swift/enumerations.playground/Contents.swift
+++ advanced_swift/enumerations.playground/Contents.swift
@@ -54,3 +54,30 @@ func checkIfCooked2(for pasta: PastaTypesA) {
   }
 }
 checkIfCooked2(for: .spaghetti(cookingTime: 7))
+
+//Work with raw and associated values
+enum Ingredients: String{
+    case lettuce = "Romaine Lettuce"
+    case croutons = "Salted Croutons"
+    case chicken = "Free Range Chicken"
+}
+
+let ingredient = Ingredients.lettuce
+
+switch ingredient {
+case .lettuce:
+  print("We use \(ingredient.rawValue) that has the richest flavor from all the lettuces")
+case .croutons:
+  print("Our \(ingredient.rawValue) makes sure we do not have to use additional salt in our salads")
+case .chicken: print("Our meals tastes better with \(ingredient.rawValue) to raise the quality")
+}
+
+enum RecipeInformation {
+  case allergens(information: String)
+}
+
+let recipeInformation = RecipeInformation.allergens(information: "Peanuts, Milk, Gluten")
+
+switch recipeInformation {
+case .allergens(let information): print("The meal includes the following allergens: \(information)")
+}
